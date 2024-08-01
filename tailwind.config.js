ca87b5cf95cdc050b7c84393d93968fec5153064
junghyunbak <jeong5728@gmail.com>
2024-08-02 02:05:21 +0900

    style: 폰트설정 (pretendard, sebang)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sebang: "sebang",
+      pretendard: "pretendard",
+    },
+    extend: {
+      colors: {
+        primary: "#6ADD76",
+        bg: "#333333",
+        "bg-secondary": "#3F3E3E",
+      },
+    },
   },
   plugins: [],
 };
