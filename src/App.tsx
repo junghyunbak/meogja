456fc4e3cc702cbdeab8f4385ae0b2dbd8758f92
junghyunbak <jeong5728@gmail.com>
2024-08-02 03:22:32 +0900

    chore: svg 설정 및 메인화면 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect } from 'react';
+import Logo from '@/assets/svgs/Logo.svg?react';
 import './App.css';
 
 import { MockApiService } from './mocking';
 import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     new MockApiService().register();
 
@@ -32,29 +29,16 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <p className="read-the-docs flex w-full">
-        Click on the Vite and React logos to learn more
+    <div className="flex flex-col items-center gap-8">
+      <Logo className="text-bg" />
+      <p>
+        함께하고 싶은 <span className="font-bold text-primary">식당</span>을
+        다같이 골라보세요.
       </p>
-    </>
+      <div className="flex w-full items-center justify-center rounded bg-primary py-2 text-white">
+        시작하기
+      </div>
+    </div>
   );
 }
 
