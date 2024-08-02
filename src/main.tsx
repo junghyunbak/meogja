695dd0aca84ea47e94056e3e83757899d7ecf2b6
junghyunbak <jeong5728@gmail.com>
2024-08-02 15:03:02 +0900

    chore: 개발환경에서 miragejs 구동

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { MockApiService } from './mocking/index.ts';
 import './index.css';
 import '@/assets/fonts/stylesheet.css';
+
+if (process.env.NODE_ENV === 'development') {
+  new MockApiService().register();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
