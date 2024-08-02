3f2f63f5cf6e60769eaa6f469c1131f9b408a5cb
junghyunbak <jeong5728@gmail.com>
2024-08-03 02:39:36 +0900

    feat: 마커 클릭 시 맵 중앙 이동 & 마커 애니메이션, 스타일 수정

--- src/pages/Room/RoomService/_components/Map/MapMarker/index.tsx
+++ src/pages/Room/RoomService/_components/Map/MapMarker/index.tsx
@@ -19,9 +19,10 @@ export function MapMarker({ map, restaurant }: MapMarkerProps) {
     const marker = new naver.maps.Marker({
       map,
       position: new naver.maps.LatLng(lat, lng),
+      animation: naver.maps.Animation.DROP,
       icon: {
         content: renderToString(
-          <div className="relative aspect-[1/1.22] w-12">
+          <div className="relative aspect-[1/1.22] w-12 -translate-x-[50%] -translate-y-full">
             <MarkerShadow className="absolute bottom-[-3px] left-[7px]" />
             <Marker className="absolute left-0 top-0 text-bg-secondary" />
             <div className="absolute left-0 top-0 flex items-center justify-center p-2.5">
@@ -33,7 +34,11 @@ export function MapMarker({ map, restaurant }: MapMarkerProps) {
     });
 
     const listener = naver.maps.Event.addListener(marker, 'click', () => {
-      setRestaurantId(restaurant.id);
+      const { id, lat, lng } = restaurant;
+
+      map.setCenter(new naver.maps.LatLng(lat, lng));
+
+      setRestaurantId(id);
     });
 
     return () => {
