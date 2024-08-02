87ec4a399d6016e6bdbbc714488ebbbbca223be6
junghyunbak <jeong5728@gmail.com>
2024-08-02 21:39:17 +0900

    feat: miragejs 방 생성 post 메서드 추가

---
+++ src/mocking/post/createRoom/index.ts
@@ -0,0 +1,20 @@
+import { type Server, Response } from 'miragejs';
+
+export function createRoom(this: Server) {
+  this.post('/api/create-room', (schema, request) => {
+    const { requestBody } = request;
+
+    const { lat, lng, capacity, minute, radius } = JSON.parse(requestBody);
+
+    if (!lat || !lng || !capacity || !minute || !radius) {
+      return new Response(400);
+    }
+
+    /**
+     * 실제로는 uuid로 식별자를 생성해야함.
+     */
+    const roomId = '0';
+
+    return new Response(201, {}, { data: { roomId } });
+  });
+}
