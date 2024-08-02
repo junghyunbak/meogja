9795e19f6a7bc5105ca372b9d3d4332235962fc9
junghyunbak <jeong5728@gmail.com>
2024-08-02 15:03:19 +0900

    feat: 임시적으로 시작하기 버튼에 navigate 기능 추가

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,17 @@
 import Logo from '@/assets/svgs/Logo.svg?react';
 import { AutoPlayFD } from '@/components/molecule/AutoPlayFD';
+import { useNavigate } from 'react-router-dom';
 
 export function Home() {
+  /**
+   * navigate: 실제론 없을 기능
+   */
+  const navigate = useNavigate();
+
+  const handleStartButtonClick = () => {
+    navigate('/room/0');
+  };
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-between bg-bg">
       <AutoPlayFD />
@@ -12,7 +22,10 @@ export function Home() {
           함께하고 싶은 <span className="font-bold text-primary">식당</span>을
           다같이 골라보세요.
         </p>
-        <div className="flex w-full cursor-pointer items-center justify-center rounded bg-primary py-2 text-white">
+        <div
+          onClick={handleStartButtonClick}
+          className="flex w-full cursor-pointer items-center justify-center rounded bg-primary py-2 text-white"
+        >
           시작하기
         </div>
 
