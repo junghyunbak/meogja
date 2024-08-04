275ef24b6e0b59b817fcf329bf4a937f43bdd049
junghyunbak <jeong5728@gmail.com>
2024-08-04 14:03:09 +0900

    refactor: 마커 객체 매번 재생성되는 문제 수정

--- src/pages/Room/RoomService/_components/Map/overlay/marker/RestaurantMarker/index.tsx
+++ src/pages/Room/RoomService/_components/Map/overlay/marker/RestaurantMarker/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { renderToString } from 'react-dom/server';
 import Chicken from '@/assets/svgs/chicken.svg?react';
 import Marker from '@/assets/svgs/marker.svg?react';
@@ -9,71 +9,79 @@ interface RestaurantMarkerProps {
   restaurant: Restaurant;
 }
 
-export const RestaurantMarker = memo(
-  ({ restaurant }: RestaurantMarkerProps) => {
-    const [map] = useStore((state) => [state.map]);
+export const RestaurantMarker = ({ restaurant }: RestaurantMarkerProps) => {
+  const [map] = useStore((state) => [state.map]);
+  const [mySelect] = useStore((state) => [state.mySelect]);
+  const [restaurantId] = useStore((state) => [state.restaurantId]);
+  const [setRestaurantId] = useStore((state) => [state.setRestaurantId]);
 
-    const [mySelect] = useStore((state) => [state.mySelect]);
+  const [marker, setMarker] = useState<naver.maps.Marker | null>(null);
 
-    const [setRestaurantId] = useStore((state) => [state.setRestaurantId]);
+  useEffect(() => {
+    if (!map) {
+      return;
+    }
 
-    const [restaurantId] = useStore((state) => [state.restaurantId]);
+    const marker = new naver.maps.Marker({
+      map,
+      position: new naver.maps.LatLng(restaurant.lat, restaurant.lng),
+      animation: naver.maps.Animation.DROP,
+      icon: {
+        content: createMarkerIcon(false, false),
+      },
+    });
 
-    /**
-     * 매번 마커객체를 재생성할 필요가 없다.
-     *
-     * // [ ]: 최적화 필요
-     */
-    useEffect(() => {
-      if (!map) {
-        return;
-      }
+    setMarker(marker);
 
+    const listener = naver.maps.Event.addListener(marker, 'click', () => {
       const { id, lat, lng } = restaurant;
 
-      const isActive = restaurantId === id;
+      map.setCenter(new naver.maps.LatLng(lat, lng));
 
-      const isSelect = mySelect.includes(id);
+      setRestaurantId(id);
+    });
 
-      const marker = new naver.maps.Marker({
-        map,
-        position: new naver.maps.LatLng(lat, lng),
-        icon: {
-          content: renderToString(
-            <div
-              className={`relative aspect-[1/1.22] -translate-x-[50%] -translate-y-full transition-all ${isActive ? 'z-50 w-14' : 'w-12'}`}
-            >
-              <MarkerShadow
-                className={`absolute bottom-0 left-[50%] ${isActive ? 'w-16' : 'w-12'}`}
-              />
-              <Marker
-                className={`absolute left-0 top-0 text-bg-secondary ${isSelect ? 'text-primary' : isActive ? 'text-bg' : ''} ${isActive ? 'stroke-white' : ''}`}
-              />
-              <div
-                className={`absolute left-0 top-0 flex items-center justify-center ${isActive ? 'p-3' : 'p-2.5'}`}
-              >
-                <Chicken className="w-full" />
-              </div>
-            </div>
-          ),
-        },
-      });
+    return () => {
+      marker.setMap(null);
 
-      const listener = naver.maps.Event.addListener(marker, 'click', () => {
-        const { id, lat, lng } = restaurant;
+      naver.maps.Event.removeListener(listener);
+    };
+  }, [map, setRestaurantId, restaurant]);
 
-        map.setCenter(new naver.maps.LatLng(lat, lng));
+  /**
+   * restaurantId가 변경될 때 마다 마커 아이콘을 변경
+   */
+  useEffect(() => {
+    if (!marker) {
+      return;
+    }
 
-        setRestaurantId(id);
-      });
+    const isActive = restaurantId === restaurant.id;
 
-      return () => {
-        marker.setMap(null);
+    const isSelect = mySelect.includes(restaurant.id);
 
-        naver.maps.Event.removeListener(listener);
-      };
-    }, [restaurantId, map, restaurant, setRestaurantId, mySelect]);
+    marker.setIcon({ content: createMarkerIcon(isActive, isSelect) });
+  }, [restaurantId, restaurant, mySelect, marker]);
 
-    return <div>{/* MapMarker 함수 컴포넌트 */}</div>;
-  }
-);
+  return null;
+};
+
+function createMarkerIcon(isActive: boolean, isSelect: boolean) {
+  return renderToString(
+    <div
+      className={`relative aspect-[1/1.22] -translate-x-[50%] -translate-y-full transition-all ${isActive ? 'z-50 w-14' : 'w-12'}`}
+    >
+      <MarkerShadow
+        className={`absolute bottom-0 left-[50%] ${isActive ? 'w-16' : 'w-12'}`}
+      />
+      <Marker
+        className={`absolute left-0 top-0 text-bg-secondary ${isSelect ? 'text-primary' : isActive ? 'text-bg' : ''} ${isActive ? 'stroke-white' : ''}`}
+      />
+      <div
+        className={`absolute left-0 top-0 flex items-center justify-center ${isActive ? 'p-3' : 'p-2.5'}`}
+      >
+        <Chicken className="w-full" />
+      </div>
+    </div>
+  );
+}
--- src/store/slices/restaurant.ts
+++ src/store/slices/restaurant.ts
@@ -1,6 +1,7 @@
 import { type StateCreator } from 'zustand';
 
 type RestaurantSlice = {
+  // [ ]: 현재 선택된 음식점 id로 네이밍 변경 필요
   restaurantId: string | null;
   setRestaurantId: (restaurantId: string | null) => void;
 };
