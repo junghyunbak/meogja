cfa6986b161d97e8b5ebd98de70d6553510b68df
junghyunbak <jeong5728@gmail.com>
2024-08-07 22:34:39 +0900

    refactor: 상수 파일 index.ts 생성

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,28 +1,3 @@
-export const LOCALSTORAGE_DUMP_KEY = 'miragejs-dump';
-
-export const RIGHT: RIGHT = 0;
-
-export const LEFT: LEFT = 1;
-
-export const NICKNAME_ADJECTIVE = [
-  '멋진',
-  '신난',
-  '춤추는',
-  '신중한',
-  '진지한',
-  '멍때리는',
-  '용감한',
-  '즐거운',
-  '행복한',
-  '피곤한',
-  '부지런한',
-  '생각하는',
-  '집중하는',
-  '외출하는',
-  '놀러가는',
-  '고민하는',
-  '고민중인',
-  '혼란스러운',
-  '맛집 찾는',
-  '먹는데 진심인',
-];
+export * from './nickname';
+export * from './room';
+export * from './api';
