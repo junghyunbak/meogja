c7382003343177b32ab7c09ec656a1c51b62c1bd
junghyunbak <jeong5728@gmail.com>
2024-08-07 23:29:21 +0900

    chore: 애니메이션 딜레이가 완료된 이후 게이지 0으로 초기화

--- src/pages/Room/_components/DelayForAnimation/index.tsx
+++ src/pages/Room/_components/DelayForAnimation/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 
+import useStore from '@/store';
+
 import { sleep } from '@/utils';
 
 interface DelayForAnimationProps {
@@ -13,12 +15,20 @@ interface DelayForAnimationProps {
 }
 
 export function DelayForAnimation({ children, delay = 1000 }: DelayForAnimationProps) {
+  const [setGage] = useStore((state) => [state.setGage]);
+
   const { isLoading } = useQuery({
     queryKey: ['delay-for-animation'],
     queryFn: async () => {
       await sleep(delay);
     },
     suspense: true,
+    onSuccess() {
+      /**
+       * 전역 상태가 살아있을것을 대비하여 0단계로 다시 초기화
+       */
+      setGage(0);
+    },
   });
 
   if (isLoading) {
