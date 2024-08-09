8afe9319d192cbde28eb47fe199d741cb23256be
junghyunbak <jeong5728@gmail.com>
2024-08-09 12:24:06 +0900

    chore: 다른 사용자의 데이터는 blocking 하지 않도록 설정

--- src/pages/Room/RoomService/_components/RefetchIntervalMutableRoomState/index.tsx
+++ src/pages/Room/RoomService/_components/RefetchIntervalMutableRoomState/index.tsx
@@ -33,17 +33,18 @@ export function RefetchIntervalMutableRoomState() {
       return { sendTime, data };
     },
     onSuccess({ sendTime, data }) {
-      // [ ]: 모든 업데이트를 막으면 부자연스러워질 수 있다. 다른 사용자들의 요청은 계속 반영되도록 하는 편이 좋겠다.
+      const { user } = data;
+
+      setUser(user);
+
       if (mutationTime.current > sendTime) {
         return;
       }
 
-      const { user } = data;
       const { select, userName } = user[userId];
 
       setMyName(userName);
       setMySelect(select);
-      setUser(user);
     },
     refetchInterval: 1000,
   });
