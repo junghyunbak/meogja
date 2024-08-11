f0e31da3434dc9bd7b4a525d5e1638beb6795ed1
junghyunbak <jeong5728@gmail.com>
2024-08-11 15:23:22 +0900

    refactor: 유틸함수로 기존 함수 대체

--- src/pages/Room/RoomService/_components/Geolocation/index.tsx
+++ src/pages/Room/RoomService/_components/Geolocation/index.tsx
@@ -1,32 +1,11 @@
 import useStore from '@/store';
 import { useEffect } from 'react';
+import { getMyLatLng } from '@/utils';
 
 export function Geolocation() {
   const [setMyGpsLatLng] = useStore((state) => [state.setMyGpsLatLng]);
 
   useEffect(() => {
-    const getMyLatLng = (): Promise<{ lat: number; lng: number } | null> => {
-      return new Promise((resolve) => {
-        if (!navigator.geolocation) {
-          resolve(null);
-        }
-
-        const successCallback: PositionCallback = (position) => {
-          const {
-            coords: { latitude, longitude },
-          } = position;
-
-          resolve({ lat: latitude, lng: longitude });
-        };
-
-        const errorCallback: PositionErrorCallback = () => {
-          resolve(null);
-        };
-
-        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-      });
-    };
-
     const updateMyLatLng = async () => {
       setMyGpsLatLng(await getMyLatLng());
     };
