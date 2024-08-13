b2d6ce2982b070aa80191f6697d44a7797b6abca
junghyunbak <jeong5728@gmail.com>
2024-08-14 02:29:42 +0900

    feat: frontend docker 설정

---
+++ frontend/docker/build-and-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+FE_IMAGE_NAME=meogja
+
+docker build --file "$SCRIPT_DIR/Dockerfile" --tag ${FE_IMAGE_NAME} "$SCRIPT_DIR/.."
+
+docker rm --force ${FE_IMAGE_NAME}
+
+docker image prune --force
+
+docker run --detach --publish 4173:4173 --name ${FE_IMAGE_NAME} ${FE_IMAGE_NAME}
