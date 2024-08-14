e792a496d96ba352176235e2ff3d925210e74a4d
junghyunbak <jeong5728@gmail.com>
2024-08-14 16:43:52 +0900

    feat: gps lat, lng 업데이트 mocking api 구현

--- frontend/src/mocking/patch/index.ts
+++ frontend/src/mocking/patch/index.ts
@@ -2,9 +2,11 @@ import { type Server } from 'miragejs';
 import { updateUserName } from './updateUserName';
 import { updateUserSelect } from './updateUserSelect';
 import { updateUserLatLng } from './updateUserLatLng';
+import { updateUserGpsLatLng } from './updateUserGpsLatLng';
 
 export const PATCH = (serverThis: Server) => {
   updateUserName.call(serverThis);
   updateUserSelect.call(serverThis);
   updateUserLatLng.call(serverThis);
+  updateUserGpsLatLng.call(serverThis);
 };
