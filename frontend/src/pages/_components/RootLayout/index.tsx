c93c6e5ae6338ba14188c9713956b4409a5c1875
junghyunbak <jeong5728@gmail.com>
2024-08-14 11:13:27 +0900

    style: root layout height 단위 변경

--- frontend/src/pages/_components/RootLayout/index.tsx
+++ frontend/src/pages/_components/RootLayout/index.tsx
@@ -5,5 +5,5 @@ interface RootLayoutProps {
 }
 
 export function RootLayout({ children }: RootLayoutProps) {
-  return <div className="mx-auto h-screen max-w-[600px]">{children}</div>;
+  return <div className="mx-auto h-dvh max-w-[600px]">{children}</div>;
 }
