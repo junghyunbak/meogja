56842ef26027dca4b33891f88c32b3fd6aa553ca
junghyunbak <jeong5728@gmail.com>
2024-08-15 16:59:51 +0900

    chore: frontend docker 스크립트 수정

--- frontend/docker/build-and-run.sh
+++ frontend/docker/build-and-run.sh
@@ -2,9 +2,9 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-FE_IMAGE_NAME=meogja
+FE_IMAGE_NAME=meogja-fe
 
-docker build --file "$SCRIPT_DIR/Dockerfile" --tag ${FE_IMAGE_NAME} "$SCRIPT_DIR/.."
+docker build --file "$SCRIPT_DIR/Dockerfile" --tag ${FE_IMAGE_NAME} "$SCRIPT_DIR/../.."
 
 docker rm --force ${FE_IMAGE_NAME}
 
